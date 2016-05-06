ad60624655c7d152fbcf795ed3c7eb0ba77f5add
John Days <johnydays0@gmail.com>
2016-05-06 12:54:13 +0100

    Only show one copy of each command. Color search. Add package.json

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
+
+'use strict'
+
+const colors = require('colors/safe')
 const execSync = require('child_process').execSync
 const readFileSync = require('fs').readFileSync
 const appendFileSync = require('fs').appendFileSync
 const homedir = require('os').homedir
+
 const run = command => execSync(command).toString("utf8").trim()
 const getIndex = item => parseInt(item.split(";")[0])
+const getCommand = item => item.split(";")[1]
+const colorize = (item, search) => {
+  const searchStart = item.indexOf(search)
+  const searchEnd = searchStart + search.length
+  return `${item.slice(0, searchStart)}${colors.red(search)}${item.slice(searchEnd)}`
+}
+
 const folder = run("pwd")
 const history = readFileSync(`${homedir()}/.directory_history`, "utf8")
   .split("\n:")
   .map((item, index) => `${index};${item}`)
 
-const directoryHistory = history
+let directoryHistory = history
   .filter(item =>
     item.split(";")[2].split(";")[0] === folder
   )
@@ -18,15 +30,24 @@ const directoryHistory = history
     `${getIndex(item)};${item.split(";")[3].split("\u0000")[0]}`
   )
 
+directoryHistory = directoryHistory.reverse().reduce((agg, item) => {
+  const itemCommand = getCommand(item)
+  if (!agg.met[itemCommand]) {
+    agg.met[itemCommand] = true
+    agg.result.push(item)
+  }
+  return agg
+}, { met: {}, result: [] }).result.reverse()
+
 const search = process.argv[2]
 const commandIndex = parseInt(process.argv[2])
 if (!isNaN(commandIndex)) {
   const requestedCommand = directoryHistory.find(item => getIndex(item) === commandIndex)
-  run(`tmux send-keys "${requestedCommand.split(";")[1].replace(/"/g, '\\"')}"`)
+  run(`tmux send-keys "${getCommand(requestedCommand).replace(/"/g, '\\"')}"`)
 }
 else {
   directoryHistory
     .slice(0, directoryHistory.length - 1)
     .filter(item => ~item.indexOf(search))
-    .map(item => console.log(item))
+    .map(item => console.log(colorize(item, search)))
 }
